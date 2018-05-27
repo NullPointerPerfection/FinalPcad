f39d364840b1768a5f87a20cb56fd80908dafd2c
Antares96 <39152829+Antares96@users.noreply.github.com>
2018-05-27 17:38:09 +0200

    deleted wrong files

--- Base.java
+++
@@ -1,8 +0,0 @@
-package progetto;
-
-import java.rmi.RemoteException;
-
-public interface Base extends Remote {
-
-	public Integer ReqConnection(String user, String server) throws RemoteException;
-}
